25b0ce68f53bf845df22f498573ecbf636b6b8a4
ZUBAER <zubaer.developer@gmail.com>
2025-10-31 17:17:51 +0600

    show and hide password setup

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,15 +2,23 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../../firebase/firebase";
 import { Link } from "react-router";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Register = () => {
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleRegister = (event) => {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
+    const terms = event.target.term.checked;
     console.log("register clicked", email, password);
+
+    // reset status: success or error
+    setError("");
+    setSuccess(false);
 
     const passwordPattern =
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*[^\w\d\s])(?=.{6,}).*$/;
@@ -24,13 +32,26 @@ const Register = () => {
       return;
     }
 
+    if (!terms) {
+      setError("Please accept our terms and conditions.");
+      return;
+    }
+
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         console.log(result);
+        setSuccess(true);
+        event.target.reset();
       })
       .catch((error) => {
         console.log(error);
       });
+  };
+
+  const handleShowHidePassword = (event) => {
+    event.preventDefault();
+    console.log("click Eye");
+    setShowPassword(!showPassword);
   };
 
   return (
@@ -51,22 +72,40 @@ const Register = () => {
                   placeholder="Email"
                 />
                 <label className="label">Password</label>
-                <input
-                  name="password"
-                  type="password"
-                  className="input"
-                  placeholder="Password"
-                />
+                <div className=" relative">
+                  <input
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    className="input"
+                    placeholder="Password"
+                  />
+                  <button
+                    onClick={handleShowHidePassword}
+                    className=" absolute right-5 top-3 text-[15px]"
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </button>
+                </div>
                 <div>
-                  <a className="link link-hover">Forgot password?</a>
+                  <div>
+                    <label class="label">
+                      <input type="checkbox" name="term" class="checkbox" />
+                      Accept Our Terms and Conditions
+                    </label>
+                  </div>
                 </div>
                 <button className="btn btn-neutral mt-4">Register</button>
               </fieldset>
+              {success && (
+                <p className=" text-green-500">Account Created Successfully</p>
+              )}
               {error && <p className=" text-red-500"> {error} </p>}
             </form>
             <p>
               Already have an account? Please{" "}
-              <Link className=" text-blue-500 underline">Login</Link>
+              <Link className=" text-blue-500 underline" to="/login">
+                Login
+              </Link>
             </p>
           </div>
         </div>
